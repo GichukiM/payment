922fd873d5e079e1536764922a3a52c1a9ab2d25
Gichuki Muchir <gichukimuchiri80@gmail.com>
2023-08-02 16:13:29 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Document</title>
+ <script src="https://unpkg.com/intasend-inlinejs-sdk@3.0.2/build/intasend-inline.js"></script>
+</head>
+<body>
+ 
+
+<button class="btn btn-primary btn-lg intaSendPayButton" data-amount="10" data-currency="KES">Pay Now</button>
+</body>
+</html>
